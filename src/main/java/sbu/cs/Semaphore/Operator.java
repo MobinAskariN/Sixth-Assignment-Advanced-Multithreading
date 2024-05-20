ec758d46d1a2b98c1cf9e1f532bdaa37c06cc1d4
MobinAskariN <160580813+MobinAskariN@users.noreply.github.com>
2024-05-20 23:45:12 +0330

    completing assignment & adding report file

--- src/main/java/sbu/cs/Semaphore/Operator.java
+++ src/main/java/sbu/cs/Semaphore/Operator.java
@@ -1,21 +1,33 @@
 package sbu.cs.Semaphore;
 
+import java.time.LocalDateTime;
+import java.util.concurrent.Semaphore;
+
 public class Operator extends Thread {
 
-    public Operator(String name) {
-        super(name);
-    }
+    private Semaphore sem;
 
+    public Operator(String name, Semaphore sem) {
+        super(name);
+        this.sem = sem;
+    }
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++)
-        {
-            Resource.accessResource();         // critical section - a Maximum of 2 operators can access the resource concurrently
-            try {
-                sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        try {
+            sem.acquire();
+            System.out.println(currentThread().getName() + " " + LocalDateTime.now());
+            for (int i = 0; i < 10; i++)
+            {
+                Resource.accessResource();         // critical section - a Maximum of 2 operators can access the resource concurrently
+                try {
+                    sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+        sem.release();
     }
 }
